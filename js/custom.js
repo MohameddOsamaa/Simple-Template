f20babaf4da8aae4095178d6d26104d970a9fa0c
MohameddOsamaa <mohamedd.osamaa@gmail.com>
2017-10-18 22:59:17 +0200

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -3,4 +3,60 @@ $ (function () {
 
 	$('html').niceScroll();
 	$('.header').height($(window).height());
+
+
+	$('.header .arrow i').click(function () {
+
+		$('html, body').animate({
+			scrollTop: $('.feature')
+			.offset().top }, 1000);
+	});
+
+	$('.hire').click(function () {
+
+		$('html, body').animate({
+			scrollTop: $('.team')
+			.offset().top }, 1000);
+	});
+
+	$('.works').click(function () {
+
+		$('html, body').animate({
+			scrollTop: $('.our-work')
+			.offset().top }, 1000);
+	});
+
+	var leftArrow = $('.Testimonials  .fa-chevron-left'),
+		rightArrow = $('.Testimonials .fa-chevron-right');
+
+	function checkClients() {
+		if ($('.client:first').hasClass('active')){
+			leftArrow.fadeOut();
+		}else{
+			leftArrow.fadeIn();
+		}
+		if ($('.client:last').hasClass('active')){
+			rightArrow.fadeOut();
+		}else {
+			rightArrow.fadeIn();
+		}
+	}
+	checkClients();
+
+	$('.Testimonials i').click(function (){
+		if ($(this).hasClass('fa-chevron-right')){
+
+			$('.Testimonials .active').fadeOut(100, function(){
+				$(this).removeClass('active').next('.client').addClass('active').fadeIn();
+				checkClients();
+			});
+
+		}else{
+
+			$('.Testimonials .active').fadeOut(100, function(){
+				$(this).removeClass('active').prev('.client').addClass('active').fadeIn();
+				checkClients();
+			});
+		}
+	});
 });
